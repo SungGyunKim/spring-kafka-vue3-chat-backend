eacf5df3972c3d9909398d1d4f9bbcc6f6956763
SungGyunKim <egoksg@naver.com>
2022-12-05 22:55:07 +0900

    프로젝트 초기화

---
+++ src/main/java/com/egoksg/SpringKafkaVue3ChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.egoksg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaVue3ChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaVue3ChatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egoksg/SpringKafkaVue3ChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egoksg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaVue3ChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
